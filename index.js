e2ff305f3b5155b3b0196c9276633d30fc989090
abdouconde <douraconde224@gmail.com>
2024-07-22 00:11:21 +0200

    Arrow function does not have own this, they are attached to the surrounded context

--- index.js
+++ index.js
@@ -1,39 +1,25 @@
-/**
- * CHALLENGE 1-2 SOLUTION
- *
- * Find all menu elements by selector ".nav-link"
- * or by className "nav-link".
- *
- *  Use "for" loop to iterate through all menu items
- * Inside of the loop add "onclick" event handler to
- * each menu item.
- *
- * In the "onclick" event handler:
- * 1. Remove first "active" class from all
- * menu elements
- * 2. Add "active" class to the clicked menu element
- * 3. Log to the console message with the name
- * of the clicked menu item
- */
+//Arrow function does not have own this, they are attached to the surrounded context
 
-
-const navBarItems = document.querySelectorAll(".nav-link");
-const LEN = navBarItems.length;
-
-console.log(navBarItems);
-
-for(let i=0;i<LEN;i++){
-  const currentItem=navBarItems[i];
-
-  currentItem.onclick=function(){
-    
-    for(let j=0;j<LEN;j++){
-      navBarItems[j].classList.remove("active");
-    }
-
-    navBarItems[i].classList.add("active");
-
-  
-  }
-
+var person1={
+    name:"Conde Abdourahamane",
+    age: 26,
+    present: ()=>{
+        return "This is "+this.name + " , He is " + this.age
+    } //This here is always global or window
 }
+
+var person2={
+    name:"Conde Abdourahamane",
+    age: 26,
+    present: function(){
+        return "This is "+this.name + " , He is " + this.age
+    } 
+}
+
+var anotherPerson={
+    name: "Conde Souleymane",
+    age: 15,
+}
+
+console.log(person1.present.call(anotherPerson)) //This is undefined , He is undefined
+console.log(person2.present.call(anotherPerson)) //This is Conde Souleymane , He is 15
