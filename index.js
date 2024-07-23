53a3d57ebc4150c74cc9c781b9e5c95f6a257240
abdouconde <douraconde224@gmail.com>
2024-07-24 00:28:54 +0200

    arrow function cannot be used as function constructor

--- index.js
+++ index.js
@@ -1,39 +1,18 @@
-//Arrow function does not have own this, they are attached to the surrounded context
+//Arrow function cannot be used as function constructor, Exception: ... Is not a constructor
 
-var person1={
-    name:"Conde Abdourahamane",
-    age: 26,
-    present: function()
-    {
-        setTimeout(function()
-        {
-            console.log("This is "+this.name + " , He is " + this.age) 
-        }.bind(this), //We need bind here because we don't use arrow function
-        2000)
-    } 
+function Pyramidian(isu, firstname, lastname){
+    this.isu=isu,
+    this.firstname=firstname,
+    this.lastname=lastname
+} // This works fine
+
+var pyramidian= new Pyramidian(1, "Souleymane", "Condé");
+
+var Pyramidien= (isu, firstname, lastname)=>{
+    this.isu=isu,
+    this.firstname=firstname,
+    this.lastname=lastname
 }
 
-var person2={
-    name:"Conde Abdourahamane",
-    age: 26,
-    present: function(){
-        setTimeout(()=>{
-            console.log("This is "+this.name + " , He is " + this.age) 
-        },2000) // Here, there is no need to bind thanks to arrow function whose this is the surronding context : person2
-    }
-}
-
-var anotherPerson1={
-    name: "Conde Souleymane",
-    age: 15,
-}
-
-var anotherPerson2={
-    name: "Conde Karifa",
-    age: 15,
-}
-
-console.log(person1.present.call(anotherPerson1)) //This is undefined , He is undefined
-console.log(person2.present.call(anotherPerson2)) //This is Conde Souleymane , He is 15
-
-
+// console.log(pyramidian)
+console.log(new Pyramidien(1, "Souleymane", "Condé"))
