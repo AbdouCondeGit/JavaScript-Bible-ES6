cc4607991ace50d6f67595237a63c8c2ebc040a1
abdouconde <douraconde224@gmail.com>
2024-07-24 00:41:06 +0200

    typeof(function instance) = function not object

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-//Arrow function cannot be used as function constructor, Exception: ... Is not a constructor
+/*typeof return's type is "string". The value is :
+    1- the underlying primitive type for primitive type operands (string, number, undefined, boolean, etc...)
+    2- "object" for all reference types 
+    3- "object" for null
+    4- "function" for functions
+    */
 
-function Pyramidian(isu, firstname, lastname){
-    this.isu=isu,
-    this.firstname=firstname,
-    this.lastname=lastname
-} // This works fine
+    // console.log(typeof(undefined)) //undefined
+    // console.log(typeof(null)) //object
+    // console.log(typeof({})) //object 
+    //  console.log(typeof([])) //object not array
+    //  console.log(typeof( ()=>{})) //function not object
+    //  console.log(typeof( function(){})) //function not object
+    //  console.log(typeof( function(){})) //function not object
 
-var pyramidian= new Pyramidian(1, "Souleymane", "Condé");
+/* Intanceof :
+    1- always return false for primitive types as they don't have function constructor in the proto list 
+    2- true if the function constructor on the right is in the proto list of the argument
 
-var Pyramidien= (isu, firstname, lastname)=>{
-    this.isu=isu,
-    this.firstname=firstname,
-    this.lastname=lastname
-}
-
-// console.log(pyramidian)
-console.log(new Pyramidien(1, "Souleymane", "Condé"))
+    
+    */
