dd5786b097b4aee76ba17cd6e13918e9c61aafd6
abdouconde <douraconde224@gmail.com>
2024-08-07 23:37:38 +0200

    this keyword okay

--- index.js
+++ index.js
@@ -1,21 +1,54 @@
-/*typeof return's type is "string". The value is :
-    1- the underlying primitive type for primitive type operands (string, number, undefined, boolean, etc...)
-    2- "object" for all reference types 
-    3- "object" for null
-    4- "function" for functions
-    */
+var papa={
+    firstname : "Kaba",
+    lastname : "Condé",
+    age: 73,
+    greeting: function(){
+        setTimeout(function(){
+            console.log("Hello my son, this is your father: "+this.lastname+" "+this.firstname+" "+this.age)
+        }.bind(this), 2000); // We need to bind because context is lost for setTimeout callback argument 
+        console.log(this)
+    }
+}
 
-    // console.log(typeof(undefined)) //undefined
-    // console.log(typeof(null)) //object
-    // console.log(typeof({})) //object 
-    //  console.log(typeof([])) //object not array
-    //  console.log(typeof( ()=>{})) //function not object
-    //  console.log(typeof( function(){})) //function not object
-    //  console.log(typeof( function(){})) //function not object
+var mama={
+    firstname : "Saran",
+    lastname : "Condé",
+    age: 55,
+    greeting: function(){
+        setTimeout(()=>{
+            console.log("Hello my son, this is your father: "+this.lastname+" "+this.firstname+" "+this.age)
+        }, 2000); // We don't need to bind because setTimeout callback argument inherits context from greeting method (mama)
+        console.log(this)
+    }
+}
 
-/* Intanceof :
-    1- always return false for primitive types as they don't have function constructor in the proto list 
-    2- true if the function constructor on the right is in the proto list of the argument
+var me={
+    firstname : "Abdourahamane",
+    lastname : "Condé",
+    age: 73,
+    greeting: function(){
+        var greet=()=>{
+        console.log("Hello my son, this is your father: "+this.lastname+" "+this.firstname+" "+this.age)
+       } // We don't need to bind because greet inherits context from greeting method (me)
 
-    
-    */
+       greet()
+    }
+}
+
+
+var bro={
+    firstname : "Souleymane",
+    lastname : "Condé",
+    age: 14,
+    greeting: function(){
+        var greet=function(){
+        console.log("Hello my son, this is your father: "+this.lastname+" "+this.firstname+" "+this.age)
+       } // We need to bind here because context is lost 
+
+       greet()
+    }
+}
+// papa.greeting()
+// mama.greeting()
+// me.greeting()
+//bro.greeting()
