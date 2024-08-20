347996a81a6c3c63f782fec3f3c4f2719b9ad9bb
abdouconde <douraconde224@gmail.com>
2024-08-20 23:21:58 +0200

    okay

--- index.js
+++ index.js
@@ -1,54 +1,21 @@
-var papa={
-    firstname : "Kaba",
-    lastname : "Condé",
-    age: 73,
-    greeting: function(){
-        setTimeout(function(){
-            console.log("Hello my son, this is your father: "+this.lastname+" "+this.firstname+" "+this.age)
-        }.bind(this), 2000); // We need to bind because context is lost for setTimeout callback argument 
-        console.log(this)
+class ButtonCreator {
+    constructor(content) {
+        this.content=content
+    }
+
+     createButton() {
+        var btn = document.createElement("button");
+        var clickCount = 0;
+        btn.innerHTML = this.content;
+        btn.addEventListener("click", () => {
+            btn.innerHTML = "button was clicked " + ++clickCount + " times";
+        });
+
+        document.body.appendChild(btn);
     }
 }
 
-var mama={
-    firstname : "Saran",
-    lastname : "Condé",
-    age: 55,
-    greeting: function(){
-        setTimeout(()=>{
-            console.log("Hello my son, this is your father: "+this.lastname+" "+this.firstname+" "+this.age)
-        }, 2000); // We don't need to bind because setTimeout callback argument inherits context from greeting method (mama)
-        console.log(this)
-    }
-}
+var buttonCreator= new ButtonCreator("Button 1")
 
-var me={
-    firstname : "Abdourahamane",
-    lastname : "Condé",
-    age: 73,
-    greeting: function(){
-        var greet=()=>{
-        console.log("Hello my son, this is your father: "+this.lastname+" "+this.firstname+" "+this.age)
-       } // We don't need to bind because greet inherits context from greeting method (me)
-
-       greet()
-    }
-}
-
-
-var bro={
-    firstname : "Souleymane",
-    lastname : "Condé",
-    age: 14,
-    greeting: function(){
-        var greet=function(){
-        console.log("Hello my son, this is your father: "+this.lastname+" "+this.firstname+" "+this.age)
-       } // We need to bind here because context is lost 
-
-       greet()
-    }
-}
-// papa.greeting()
-// mama.greeting()
-// me.greeting()
-//bro.greeting()
+buttonCreator.createButton()
+buttonCreator.createButton()
