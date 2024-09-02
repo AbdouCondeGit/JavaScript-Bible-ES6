68510b5cbdbfa04bd7b75cc1ce1fa8e9e114a12c
abdouconde <douraconde224@gmail.com>
2024-09-02 21:56:30 +0200

    my solution (only using some)

--- index.js
+++ index.js
@@ -1,89 +1,40 @@
-class ButtonCreator {
-    constructor(content) {
-        this.content=content
+const a= ["abdou", 4, 9, 10, null]
+const b= [10, 4, 9, 15, 100]
+const c= [150,120,110,80]
+const d= [1,2,5,100,1000]
+
+const isSortedInAscending= (array)=>{
+    const length=array.length
+    for(let index=0;index<length-1;index++){
+        if(array[index]>array[index+1]){
+            return false;
+        }
     }
+    return true;
+}
 
-     createButton() {
-        var btn = document.createElement("button");
-        var clickCount = 0;
-        btn.innerHTML = this.content;
-        btn.addEventListener("click", () => {
-            btn.innerHTML = "button was clicked " + ++clickCount + " times";
-        });
+const isSortedInDescending= (array)=>{
+    const length=array.length
+    for(let index=0;index<length-1;index++){
+        if(array[index]<array[index+1]){
+            return false;
+        }
+    }
+    return true;
+}
 
-        document.body.appendChild(btn);
+const arrayChecker= (array)=>{
+    if(array.some(element=>typeof(element)!="number")){
+        return "Some elements are not numbers";
+    }
+    
+    if(isSortedInAscending(array)){
+        return "Array is sorted in ascending order";
+    }
+  
+    if(isSortedInDescending(array)){
+        return "Array is sorted in descending order";
     }
 }
 
-var buttonCreator= new ButtonCreator("Button 1")
-
-// buttonCreator.createButton()
-// buttonCreator.createButton()
-
-const myCities= ["Conakry", "Kouroussa","Singapore"]
-const prereRegisteredStudents= [
-    {
-   "isu": "0JFBELHFKV834798",
-   "name": "Conde Souleymane"
-    },  
-    {
-   "isu": "9HLJR3FLK48943R7",
-   "name": "Conde Karifa"
-    },
-    {
-   "isu": "0943875683483479",
-   "name": "Keita Moussa"
-    },
-    {
-   "isu": "90348789FBF94844",
-   "name": "Conde Kaba"
-    }
-]
-const provideElementInfo= (element, index)=>{
-    return element+ " is located at the index "+ index+ " in the myCities array"
-}
-
-const convertElementToObject= element=>({
-    isu: element.isu,
-    name : element.name
-})
-
-// (function printArrayInfo(){
-//     for(let i=0;i<myCities.length; i++){
-//         console.log(arrayInfo(i));
-//     }
-// })()
-
-console.log("                                                               ####################################")
-
-// myCities.forEach((element, index)=>console.log(provideElementInfo(element,index)))
-const prereRegisteredStudentsArray=prereRegisteredStudents.map(convertElementToObject)
-// const prereRegisteredStudentsArray1=prereRegisteredStudents.map(element=>JSON.parse(element))
-// console.log(prereRegisteredStudentsArray)
-// console.log(prereRegisteredStudentsArray1)
-
-const items=[
-    {
-        name: "iphoneX",
-        price: 450,
-        count: 10
-    },
-    {
-        name: "macbook pro 13",
-        price: 1000,
-        count: 15
-    },
-    {
-        name: "airpod pro",
-        price: 220,
-        count: 1
-    },
-    {
-        name: "apple watch",
-        price: 300,
-        count: 0
-    },
-]
-
-console.log(items.some(item=>item.count>5))
-
+console.log(arrayChecker(d))
